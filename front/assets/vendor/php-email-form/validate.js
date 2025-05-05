89d1706e66cd0e5976fb295e35ed096fd431caaf
Mark Christian Javes Lariosa <marklariosa18@gmail.com>
2025-05-05 19:16:48 +0800

    add

--- front/assets/vendor/php-email-form/validate.js
+++ front/assets/vendor/php-email-form/validate.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Fetch CSRF token
   async function fetchCsrfToken() {
     try {
-      const response = await fetch('/csrf-token', {
+      const response = await fetch('auth/csrf-token', {
         method: 'GET',
         credentials: 'include',
       });
