11193ef44bf8d200c37bd24339e1d30e32ae406d
Mark Christian Javes Lariosa <marklariosa18@gmail.com>
2025-05-06 06:47:49 +0800

    update

--- routes/auth.js
+++ routes/auth.js
@@ -59,7 +59,7 @@ function encryptEmail(email) {
 
 function decryptEmail(encryptedEmail) {
   try {
-    const [ivHex, authTagHex, encryptedHex] = encryptedEmail.split(':');
+    const [ivText, encryptedText, authTagText] = text.split(':');
     if (!ivHex || !authTagHex || !encryptedHex) {
       throw new Error('Invalid encrypted email format');
     }
